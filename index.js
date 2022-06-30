f68ba779c9dbf55af25d33fdca8fbfb1417877e9
Cynthia <67034552+ccy05327@users.noreply.github.com>
2022-06-30 20:09:01 +0800

    Fetch JSON

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const app = express();
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
-
-const file = "D:\\GitHub\\Sleep_Data_Visualization\\SDV.json";
+const { stringify } = require("querystring");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+// const file = "D:\\GitHub\\Sleep_Data_Visualization\\SDV.json";
 // const file = "https:\\\\github.com\\ccy05327\\Sleep-Data-Visualization\\blob\\main\\SDV.json";
-// const file = "https:\\ccy05327.github.io\\Sleep-Data-Visualization\\SDV.json";
+const file = "https:\\ccy05327.github.io\\Sleep-Data-Visualization\\SDV.json";
 // const file = "https://raw.githubusercontent.com/ccy05327/Sleep-Data-Visualization/main/SDV.json";
-
+// D:\GitHub\SDV-API\index.js
 app.use(cors());
 app.use(express.json());
 
@@ -20,34 +22,50 @@ function past_record(n) {
 }
 
 // Data Processing
-let data = Object.values(require(file));
+// let data = Object.values(require(file));
 
 // https://nodejs.org/en/knowledge/HTTP/clients/how-to-access-query-string-parameters/
 http
   .createServer((req, res) => {
     const queryObject = url.parse(req.url, true).query;
-    console.log(queryObject);
-    fs.readFile(file, (err, content) => {
-      if (err) {
-        if (err.code == "ENOENT") {
-          res.writeHead(404);
-          res.end(err.code);
-        } else {
-          res.writeHead(500);
-          res.end(err.code);
-        }
-      } else {
-        res.writeHead(200, {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "X-Powered-By": "nodejs",
-        });
-        if ("past" in queryObject) {
-          data = past_record(parseInt(queryObject.past));
-          console.log(parseInt(queryObject.past));
-          res.end(data);
-        }
-      }
+    // console.log(queryObject);
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "X-Powered-By": "nodejs",
     });
+    if ("past" in queryObject) {
+      fetch(file)
+        .then((res) => res.json())
+        .then((data) => {
+          data = data.sleep_record.slice(-parseInt(queryObject.past) - 2, -2);
+          data = JSON.stringify(data);
+          console.log(queryObject, parseInt(queryObject.past));
+          res.end(data);
+        });
+    }
+
+    // fs.readFile(file, (err, content) => {
+    //   if (err) {
+    //     if (err.code == "ENOENT") {
+    //       res.writeHead(404);
+    //       res.end(err.code);
+    //     } else {
+    //       res.writeHead(500);
+    //       res.end(err.code);
+    //     }
+    //   } else {
+    // res.writeHead(200, {
+    //   "Content-Type": "application/json",
+    //   "Access-Control-Allow-Origin": "*",
+    //   "X-Powered-By": "nodejs",
+    // });
+    //     if ("past" in queryObject) {
+    //       data = past_record(parseInt(queryObject.past));
+    //       console.log(parseInt(queryObject.past));
+    //       res.end(data);
+    //     }
+    //   }
+    // });
   })
   .listen(8080);
