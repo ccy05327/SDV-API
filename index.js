33826788ec4d007a6bba3416a1d2ed08b322ef1a
Cynthia <67034552+ccy05327@users.noreply.github.com>
2022-06-30 20:42:04 +0800

    port update

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ const http = require("http");
 const url = require("url");
 const { stringify } = require("querystring");
 const fetch = (...args) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...args));
+    import("node-fetch").then(({ default: fetch }) => fetch(...args));
+  
 // const file = "D:\\GitHub\\Sleep_Data_Visualization\\SDV.json";
 // const file = "https:\\\\github.com\\ccy05327\\Sleep-Data-Visualization\\blob\\main\\SDV.json";
 const file = "https:\\ccy05327.github.io\\Sleep-Data-Visualization\\SDV.json";
 // const file = "https://raw.githubusercontent.com/ccy05327/Sleep-Data-Visualization/main/SDV.json";
 // D:\GitHub\SDV-API\index.js
+
+const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.json());
 
@@ -21,8 +25,6 @@ function past_record(n) {
   return data;
 }
 
-// Data Processing
-// let data = Object.values(require(file));
 
 // https://nodejs.org/en/knowledge/HTTP/clients/how-to-access-query-string-parameters/
 http
@@ -43,6 +45,10 @@ http
           console.log(queryObject, parseInt(queryObject.past));
           res.end(data);
         });
+    } else {
+      fetch(file)
+        .then((res) => res.json())
+        .then((data) => res.end(JSON.stringify(data)));
     }
 
     // fs.readFile(file, (err, content) => {
@@ -68,4 +74,4 @@ http
     //   }
     // });
   })
-  .listen(8080);
+  .listen(PORT);
