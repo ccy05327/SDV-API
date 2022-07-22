578dea526c0a12c27d1c93a0fc67ed32585a320e
Cynthia <67034552+ccy05327@users.noreply.github.com>
2022-07-23 00:30:38 +0800

    error checking commit

--- plotlyflask/plotlydash/dashboard.py
+++ plotlyflask/plotlydash/dashboard.py
@@ -1,38 +1,42 @@
 from dash import dash, Dash, html, dcc
+from flask import request, session
 import plotly.express as px
 import requests
-import cgi
 
-API = "http://sdv-api.herokuapp.com/"
+# from plotlyflask.routes import number_of_records
 
 
-######## Find the inputs in html and request it from API or generate v2 ########
-form = cgi.FieldStorage()
-number_of_records = form.getvalue('number_records') or 0
-print(number_of_records)
+def SDV(number_of_records):
+    API = "http://sdv-api.herokuapp.com/"
 
-######## V2 SDV - read JSON -> df -> plotly timeline ########
-records = requests.get(API).json()
-df = []
-for i in records['sleep_record'][-number_of_records:]:
-    record = dict(Date='{}/{}'.format(i['date']['month'], i['date']['day']),
-                  Sleep='2022-02-01 {}:{}:00'.format(
-        i['sleep']['hour'], i['sleep']['min']),
-        Wake='2022-02-01 {}:{}:00'.format(
-        i['wake']['hour'], i['wake']['min']),
-        Duration=i['duration'])
-    df.append(record)
+    ######## Find the inputs in html and request it from API or generate v2 ########
+    # number_of_records = request.args.get('records') or 0
+    print(number_of_records)
 
-fig = px.timeline(df, color="Duration", x_start="Sleep",
-                  x_end="Wake", y="Date", color_continuous_scale=['#ffff3f', '#52b69a', '#0077b6'],
-                  height=20*len(records['sleep_record']), width=800)
-fig.update_yaxes(autorange="reversed")
+    ######## V2 SDV - read JSON -> df -> plotly timeline ########
+    records = requests.get(API).json()
+    df = []
+    for i in records['sleep_record'][-number_of_records:]:
+        record = dict(Date='{}/{}'.format(i['date']['month'], i['date']['day']),
+                      Sleep='2022-02-01 {}:{}:00'.format(
+            i['sleep']['hour'], i['sleep']['min']),
+            Wake='2022-02-01 {}:{}:00'.format(
+            i['wake']['hour'], i['wake']['min']),
+            Duration=i['duration'])
+        df.append(record)
+    global fig
+    fig = px.timeline(df, color="Duration", x_start="Sleep",
+                      x_end="Wake", y="Date", color_continuous_scale=['#ffff3f', '#52b69a', '#0077b6'],
+                      height=20*len(records['sleep_record']), width=800)
+    fig.update_yaxes(autorange="reversed")
 
 
 def init_dashboard(server):
+    # number_of_records = request.args['messages']
+    # number_of_records = session['messages']
     dash_app = dash.Dash(
         server=server,
-        routes_pathname_prefix='/dashapp/'
+        routes_pathname_prefix='/dashapp/<length>/'
     )
 
     dash_app.layout = html.Div(children=[
--- plotlyflask/routes.py
+++ plotlyflask/routes.py
@@ -1,8 +1,13 @@
-from flask import render_template
+from flask import redirect, render_template, url_for, request, session
 from flask import current_app as app
 import requests
 
+#!\GitHub\SDV-API\plotlyflask\plotlydash
+# from plotlydash import dashboard
+from plotlydash import test
+
 API = "http://sdv-api.herokuapp.com/"
+# print(test.a)
 
 
 @app.route('/')
@@ -12,11 +17,23 @@ def home():
         title="Plotly Dash in Flask",
         description="Embed Plotly Dash into Flask application",
         template="home-template",
-        body="This is a homepage servef with Flask"
+        body="This is a homepage served with Flask"
     )
 
 
-@app.route('/json')
-def json_data():
-    response = requests.get(API).json()
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    # global number_of_records
+    number_of_records = request.form.get('number_records')
+
+    if int(request.form.get('format_selection')) == 1:  # JSON
+        return redirect(url_for('json_data', length=number_of_records))
+    else:
+        SDV(number_of_records)
+        return redirect(url_for('/dashapp/<length>/', length=number_of_records))
+
+
+@app.route('/json/<length>', methods=['GET', 'POST'])
+def json_data(length=0):
+    response = requests.get(API + str(length)).json()
     return response
